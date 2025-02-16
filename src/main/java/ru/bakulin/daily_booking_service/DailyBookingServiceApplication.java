017c53056c70f480f6164d89fde4da864b18990e
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-02-16 13:09:44 +0700

    git init

---
+++ src/main/java/ru/bakulin/daily_booking_service/DailyBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.bakulin.daily_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBookingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DailyBookingServiceApplication.class, args);
+  }
+
+}
