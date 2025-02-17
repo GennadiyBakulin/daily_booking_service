a14109575fbe49f267435abfd8f37b3585aa38aa
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-02-17 16:52:50 +0700

    Добавил описание комнат в enum RoomsType

--- src/main/java/ru/bakulin/daily_booking_service/entity/RoomsType.java
+++ src/main/java/ru/bakulin/daily_booking_service/entity/RoomsType.java
@@ -1,9 +1,15 @@
 package ru.bakulin.daily_booking_service.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum RoomsType {
-  ONLY_ROOM,
-  ONE_ROOM,
-  TWO_ROOM,
-  THREE_ROOM,
-  FOUR_OR_MORE_ROOM
+  STUDIO("Студия"),
+  ONE_ROOM("Одна комната"),
+  TWO_ROOM("Две комнаты"),
+  THREE_ROOM("Три комнаты"),
+  FOUR_OR_MORE_ROOM("Четыре и более комнат");
+  private final String description;
 }
