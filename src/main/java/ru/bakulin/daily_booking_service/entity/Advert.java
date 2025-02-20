0057564a9eeb11372e78483c32a077b75c09359e
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-02-20 21:08:29 +0700

    Установил двухстороннюю связь @ManyToOne между Advert и Apartment

--- src/main/java/ru/bakulin/daily_booking_service/entity/Advert.java
+++ src/main/java/ru/bakulin/daily_booking_service/entity/Advert.java
@@ -5,21 +5,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
 @ToString
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Advert {
 
   @Id
@@ -32,9 +32,18 @@ public class Advert {
   @Column(nullable = false)
   private Boolean isActive;
 
-  @Column(nullable = false)
-  private Integer apartmentId;
+  @ToString.Exclude
+  @ManyToOne
+  @JoinColumn(name = "apartment_id", nullable = false)
+  private Apartment apartment;
 
   @Column(nullable = false)
   private String description;
+
+  public Advert(BigDecimal price, Boolean isActive, Apartment apartment, String description) {
+    this.price = price;
+    this.isActive = isActive;
+    this.apartment = apartment;
+    this.description = description;
+  }
 }
