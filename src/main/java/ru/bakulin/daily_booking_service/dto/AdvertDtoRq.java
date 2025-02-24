fd35f9856f0da54fadc24a1a10ab365c0c8161ae
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-02-24 21:28:10 +0700

    Убрал аннотацию @JsonIgnoreProperties(ignoreUnknown = true) полей id из dto классов запросов

--- src/main/java/ru/bakulin/daily_booking_service/dto/AdvertDtoRq.java
+++ src/main/java/ru/bakulin/daily_booking_service/dto/AdvertDtoRq.java
@@ -1,13 +1,11 @@
 package ru.bakulin.daily_booking_service.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class AdvertDtoRq {
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
   private Integer id;
   private BigDecimal price;
   private Boolean isActive;
--- src/main/java/ru/bakulin/daily_booking_service/dto/ApartmentDto.java
+++ src/main/java/ru/bakulin/daily_booking_service/dto/ApartmentDto.java
@@ -1,13 +1,11 @@
 package ru.bakulin.daily_booking_service.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import ru.bakulin.daily_booking_service.entity.ApartmentType;
 
 @Data
 public class ApartmentDto {
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
   private Integer id;
   private String city;
   private String street;
