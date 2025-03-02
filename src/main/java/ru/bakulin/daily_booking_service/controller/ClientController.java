d863920d8b1dc2c003e3ee97cbb69b82106d6531
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-03-02 17:15:29 +0700

    Описание  Swagger для ClientController

--- src/main/java/ru/bakulin/daily_booking_service/controller/ClientController.java
+++ src/main/java/ru/bakulin/daily_booking_service/controller/ClientController.java
@@ -1,5 +1,8 @@
 package ru.bakulin.daily_booking_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.bakulin.daily_booking_service.service.ClientService;
 
+@Tag(name = "Client Controller",
+    description = "API для удаления клиентов")
 @RestController
 @RequestMapping("/client")
 @RequiredArgsConstructor
@@ -14,6 +19,13 @@ public class ClientController {
 
   private final ClientService service;
 
+  @Operation(
+      summary = "Удаление клиента из БД",
+      description = "Удаляет клиента и его брони из БД по переданному Id клиента")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Успешное удаление клиента из БД"
+  )
   @DeleteMapping("/{id}")
   public void deleteClient(@PathVariable Integer id) {
     service.delete(id);
