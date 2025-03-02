f46bbd26e1c588dfec367ba46ac0c2f4096d9c35
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-03-02 17:09:14 +0700

    Описание  Swagger для ApartmentController

--- src/main/java/ru/bakulin/daily_booking_service/controller/ApartmentController.java
+++ src/main/java/ru/bakulin/daily_booking_service/controller/ApartmentController.java
@@ -1,5 +1,11 @@
 package ru.bakulin.daily_booking_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.bakulin.daily_booking_service.dto.ApartmentDto;
 import ru.bakulin.daily_booking_service.service.ApartmentService;
 
+@Tag(name = "Apartment Controller",
+    description = "API для создания помещений")
 @RestController
 @RequestMapping("/apartment")
 @RequiredArgsConstructor
@@ -15,6 +23,18 @@ public class ApartmentController {
 
   private final ApartmentService service;
 
+  @Operation(
+      summary = "Создание нового помещения в БД",
+      description = "Возвращает вновь созданное помещение в БД")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Успешное сохранение помещения в БД",
+      content = {
+          @Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = ApartmentDto.class))
+          )
+      }
+  )
   @PostMapping
   public ApartmentDto create(@RequestBody ApartmentDto dto) {
     return service.save(dto);
